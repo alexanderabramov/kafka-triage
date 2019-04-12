f8c434690de342d820609fdce6e0017965569e91
Alexander Abramov <alexander.abramov.pub@gmail.com>
2019-04-12 22:15:17 +0200

    Change integration tests to use a real PostgreSQL in docker
    To properly test serializable transactions

--- src/test/kotlin/org/kafkatriage/NonTransactionalTests.kt
+++ src/test/kotlin/org/kafkatriage/NonTransactionalTests.kt
@@ -2,37 +2,25 @@ package org.kafkatriage
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import org.apache.kafka.clients.admin.AdminClient
-import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.TopicPartition
 import org.apache.logging.log4j.LogManager
-import org.assertj.core.api.Assertions.assertThat
 import org.hibernate.exception.LockAcquisitionException
 import org.kafkatriage.records.Record
-import org.kafkatriage.records.RecordController
 import org.kafkatriage.records.RecordRepository
 import org.kafkatriage.records.ReplayResult
-import org.kafkatriage.topics.TopicController
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import java.lang.Thread.sleep
 import java.util.*
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
-import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
-
-class IntegrationTest @Autowired constructor(
-        private val kafkaTemplate: KafkaTemplate<String, ByteArray>,
+@DirtiesContext
+class NonTransactionalTests @Autowired constructor(
         private val recordRepository: RecordRepository,
-        private val kafkaAdminClient: AdminClient,
-        private val topicController: TopicController,
-        private val recordController: RecordController,
         private val mockMvc: MockMvc,
         private val objectMapper: ObjectMapper
 ) : FullContextTest() {
@@ -40,34 +28,11 @@ class IntegrationTest @Autowired constructor(
     private val logger = LogManager.getLogger()
 
     @Test
-    fun `kafka record is read into db and offset committed`() {
-        val value = "value1"
-        val valueAsByteArray = value.toByteArray()
-        val producerRecord = ProducerRecord("error-test", 0, "key1", valueAsByteArray)
-        kafkaTemplate.send(producerRecord).get()
-
-        sleep(1000)
-
-        val allRecordsInDb = recordRepository.findAll()
-        assertThat(allRecordsInDb).hasOnlyOneElementSatisfying { it.topic == "error-test" && it.partition == 0 && it.offset == 0L && it.key == "key1" && it.value == value }
-
-        val offsets = kafkaAdminClient.listConsumerGroupOffsets("triage").partitionsToOffsetAndMetadata().get()
-        val offset = offsets[TopicPartition("error-test", 0)]!!.offset()
-        assertThat(offset).isEqualTo(1)
-
-        val topics = topicController.list()
-        assertThat(topics).hasOnlyOneElementSatisfying { it.name == "error-test" && it.lag == 1L }
-
-        val records = recordController.list("error-test")
-        assertThat(records).hasOnlyOneElementSatisfying { it.topic == "error-test" && it.partition == 0 && it.offset == 0L && it.key == "key1" && it.value == value }
-    }
-
-    @Test
-    @Ignore("embedded H2 does not support serializable transactions, but PostgreSQL does")
     fun `concurrent replay does not duplicate`() {
 
         val testRecord = Record(topic = "error-test", partition = 0, offset = 0, key = "", value = "")
         recordRepository.save(testRecord)
+        recordRepository.flush()
 
         val numThreads = 10
         val maxTimeoutSeconds = 2L
---
+++ src/test/kotlin/org/kafkatriage/TransactionalTests.kt
@@ -0,0 +1,48 @@
+package org.kafkatriage
+
+import org.apache.kafka.clients.admin.AdminClient
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.TopicPartition
+import org.assertj.core.api.Assertions.assertThat
+import org.kafkatriage.records.RecordController
+import org.kafkatriage.records.RecordRepository
+import org.kafkatriage.topics.TopicController
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.transaction.annotation.Transactional
+import java.lang.Thread.sleep
+import kotlin.test.Test
+
+
+@Transactional
+class TransactionalTests @Autowired constructor(
+        private val kafkaTemplate: KafkaTemplate<String, ByteArray>,
+        private val recordRepository: RecordRepository,
+        private val kafkaAdminClient: AdminClient,
+        private val topicController: TopicController,
+        private val recordController: RecordController
+) : FullContextTest() {
+
+    @Test
+    fun `kafka record is read into db and offset committed`() {
+        val value = "value1"
+        val valueAsByteArray = value.toByteArray()
+        val producerRecord = ProducerRecord("error-test", 0, "key1", valueAsByteArray)
+        kafkaTemplate.send(producerRecord).get()
+
+        sleep(1000)
+
+        val allRecordsInDb = recordRepository.findAll()
+        assertThat(allRecordsInDb).hasOnlyOneElementSatisfying { it.topic == "error-test" && it.partition == 0 && it.offset == 0L && it.key == "key1" && it.value == value }
+
+        val offsets = kafkaAdminClient.listConsumerGroupOffsets("triage").partitionsToOffsetAndMetadata().get()
+        val offset = offsets[TopicPartition("error-test", 0)]!!.offset()
+        assertThat(offset).isEqualTo(1)
+
+        val topics = topicController.list()
+        assertThat(topics).hasOnlyOneElementSatisfying { it.name == "error-test" && it.lag == 1L }
+
+        val records = recordController.list("error-test")
+        assertThat(records).hasOnlyOneElementSatisfying { it.topic == "error-test" && it.partition == 0 && it.offset == 0L && it.key == "key1" && it.value == value }
+    }
+}
